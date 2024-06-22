8c3e1d2307e54011437ac0aed54b7a2ea74b7284
gab-braga <gabrielbraga0712@gmail.com>
2024-06-22 10:34:35 -0300

    commit initial

---
+++ src/main/java/io/github/gab_braga/image_descriptor/AiImageDescriptorApplication.java
@@ -0,0 +1,13 @@
+package io.github.gab_braga.image_descriptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiImageDescriptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiImageDescriptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gab_braga/image_descriptor/AiImageDescriptorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gab_braga.image_descriptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiImageDescriptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
